c3b517f90b5f24b42a005f7c75cde9d0fd9208e3
Junior <joseinadilsonjunior@gmail.com>
2023-09-08 13:28:10 -0300

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Junior337-portifolio2.0",
   plugins: [react()],
+  base: "/Junior337-portifolio2.0",
 })
